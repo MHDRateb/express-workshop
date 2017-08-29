17bb0f1b9a1896e85b4db5323bd6361a678ec9f5
MHDRateb <ratebalissa@gmail.com>
2017-08-29 17:07:39 +0100

    replace old code with new one

--- server.js
+++ server.js
@@ -7,18 +7,38 @@ app.use(express.static("public"));
 app.use(formidable());
 
 app.post("/create-post", function (req, res) {
-  const filePath = __dirname + '/data/posts.json';
-  // console.log(req.fields.blogpost);
-  const postContent = fs.readFileSync(filePath);
-  //  console.log(postContent);
-  const posts = JSON.parse(postContent);
- // console.log(posts);
-    posts[Date.now()]=req.fields.blogpost;
-  //  //console.log(posts);
-  fs.writeFileSync(filePath,JSON.stringify(posts));
-   res.send(200,posts);
+  filePath = __dirname + '/data/posts.json';
+  fs.readFile(filePath, function (error, file) {
+      const posts = JSON.parse(file);
+      posts[Date.now()] = req.fields.blogpost;
+      fs.writeFile(filePath, JSON.stringify(posts), function (error) {
+          res.send(200, posts);
+      });        
+  });
 
 });
+
+
+
+
+
+
+
+
+
+// app.post("/create-post", function (req, res) {
+//   const filePath = __dirname + '/data/posts.json';
+//   // console.log(req.fields.blogpost);
+//   const postContent = fs.readFileSync(filePath);
+//   //  console.log(postContent);
+//   const posts = JSON.parse(postContent);
+//  // console.log(posts);
+//     posts[Date.now()]=req.fields.blogpost;
+//   //  //console.log(posts);
+//   fs.writeFileSync(filePath,JSON.stringify(posts));
+//    res.send(200,posts);
+
+// });
 
 // app.get("/get-posts", function (req, res) {
 // res.sendFile(__dirname + '/data/posts.json');
